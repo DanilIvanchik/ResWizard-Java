8904fd782c1f22c57a0c0b1faca28ad17b8718c3
danil <ivanchikdanil@gmail.com>
2023-08-26 17:35:33 +0300

    Firs commit of ResWizard!

---
+++ src/main/java/com/example/reswizard/ResWizardApplication.java
@@ -0,0 +1,13 @@
+package com.example.reswizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResWizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResWizardApplication.class, args);
+    }
+
+}
