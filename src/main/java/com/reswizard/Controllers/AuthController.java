a3723bb5926344de7afc4b49869bca5f8ca14ce6
danil <ivanchikdanil@gmail.com>
2023-10-09 13:27:45 +0300

    Added comments to methods in authentication controller.

--- src/main/java/com/reswizard/Controllers/AuthController.java
+++ src/main/java/com/reswizard/Controllers/AuthController.java
@@ -43,15 +43,28 @@ public class AuthController {
         this.userService = userService;
     }
 
+    /**
+     * Displays the login page.
+     *
+     * @param authenticationDTO The object containing authentication data.
+     * @return The login page view.
+     */
     @GetMapping("/login")
     public String loginPage(@ModelAttribute("authenticationDTO") AuthenticationDTO authenticationDTO) {
         logger.log(Level.INFO, "Displaying login page.");
         return "LoginPage";
     }
 
+    /**
+     * Processes a user login request.
+     *
+     * @param authenticationDTO The object containing authentication data.
+     * @param bindingResult     The result of data binding and validation.
+     * @return Redirects to the "process_login" endpoint if successful, otherwise returns to the login page.
+     */
     @PostMapping("/login")
     public String loginPage(@ModelAttribute("authenticationDTO") @Valid AuthenticationDTO authenticationDTO,
-            BindingResult bindingResult){
+                            BindingResult bindingResult) {
         logger.log(Level.INFO, "Processing user login.");
 
         authValidator.validate(authenticationDTO, bindingResult);
@@ -64,22 +77,39 @@ public class AuthController {
         return "redirect:/process_login";
     }
 
+    /**
+     * Displays the registration page.
+     *
+     * @param userDTO The object containing user registration data.
+     * @return The registration page view.
+     */
     @GetMapping("/registration")
     public String registrationPage(@ModelAttribute("userDTO") UserDTO userDTO) {
         logger.log(Level.INFO, "Displaying registration page.");
         return "RegistrationPage";
     }
 
+    /**
+     * Displays the access denied page.
+     *
+     * @return The access denied page view.
+     */
     @GetMapping("/access-denied")
     public String accessDenied() {
         logger.log(Level.INFO, "Displaying access denied page.");
         return "AccessDeniedEmailPage";
     }
 
+    /**
+     * Processes a user registration request.
+     *
+     * @param userDTO       The object containing user registration data.
+     * @param bindingResult The result of data binding and validation.
+     * @return Redirects to the login page if registration is successful, otherwise returns to the registration page.
+     */
     @PostMapping("/registration")
-    public String performRegistration(
-            @ModelAttribute("userDTO") @Valid UserDTO userDTO,
-            BindingResult bindingResult){
+    public String performRegistration(@ModelAttribute("userDTO") @Valid UserDTO userDTO,
+                                      BindingResult bindingResult) {
         logger.log(Level.INFO, "Processing user registration.");
 
         User user = DTOToUser(userDTO);
@@ -96,17 +126,31 @@ public class AuthController {
         return "redirect:/auth/login";
     }
 
+    /**
+     * Converts a UserDTO object to a User object.
+     *
+     * @param userDTO The UserDTO object to be converted.
+     * @return The converted User object.
+     */
     private User DTOToUser(UserDTO userDTO) {
         User user = modelMapper.map(userDTO, User.class);
         logger.log(Level.INFO, "Converting UserDTO to User object: " + user.getUsername());
         return user;
     }
 
+    /**
+     * Activates a user account with the provided activation code.
+     *
+     * @param authenticationDTO The object containing authentication data.
+     * @param model             The model to add attributes.
+     * @param code              The activation code.
+     * @return Redirects to the login page with a success or error message.
+     */
     @GetMapping("/activate/{code}")
-    public String activateUser(@ModelAttribute("authenticationDTO") AuthenticationDTO authenticationDTO, Model model, @PathVariable String code ){
-        if (userService.isActiveUser(code)){
+    public String activateUser(@ModelAttribute("authenticationDTO") AuthenticationDTO authenticationDTO, Model model, @PathVariable String code) {
+        if (userService.isActiveUser(code)) {
             model.addAttribute("message", "User account successfully activated!");
-        }else{
+        } else {
             model.addAttribute("message", "Activation code is not found.");
         }
         return "LoginPage";
